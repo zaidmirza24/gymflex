71c6f1e7ba72d4dcf4487653ae4f7b31cd05d0f7
zaidmirza24 <mirzamohdzaid72@gmail.com>
2025-05-19 13:50:16 +0530

    testimonal data changes

--- src/data/testimonialsData.js
+++ src/data/testimonialsData.js
@@ -15,56 +15,56 @@ export const testimonialsData = [
     image: image1,
     review:
       "I made the right choice by choosing the Fitclub and by choosing the right plan and program I already achieved my ideal body!",
-    name: 'MATHEW HENDRICKSON',
+    name: 'Kuntala',
     status: 'ENTREPRENEUR'
   },
   {
     image: image2,
     review:
       "The trainers here pushed me beyond my limits. I feel stronger, healthier, and more confident than ever before!",
-    name: 'SARA JAMES',
+    name: 'Niketan dadu',
     status: 'MARKETING MANAGER'
   },
   {
     image: image3,
     review:
       "Joining Fitclub was the best decision for my health. The energy here is amazing and the results speak for themselves.",
-    name: 'DAVID LEE',
+    name: 'Hero',
     status: 'SOFTWARE ENGINEER'
   },
   {
     image: image4,
     review:
       "I’ve tried several gyms, but none compare to Fitclub. The customized plans really helped me stay consistent.",
-    name: 'EMILY WATSON',
+    name: 'Aaryaman',
     status: 'TEACHER'
   },
   {
     image: image5,
     review:
       "After just 3 months at Fitclub, my stamina and physique have improved tremendously. Highly recommended!",
-    name: 'JAMES MILLER',
+    name: 'Rajesh',
     status: 'FITNESS BLOGGER'
   },
   {
     image: image6,
     review:
       "What I love most is the community vibe. Everyone motivates each other. It’s more than just a gym.",
-    name: 'LINDA FERNANDEZ',
+    name: 'Dev',
     status: 'NUTRITIONIST'
   },
   {
     image: image7,
     review:
       "Thanks to Fitclub, I’ve lost 10kg and feel better than I have in years. The support system is amazing!",
-    name: 'RAVI SHANKAR',
+    name: 'Sarvesh',
     status: 'STUDENT'
   },
   {
     image: image8,
     review:
       "Whether you’re a beginner or experienced, the team at Fitclub knows how to get you to your goals safely and effectively.",
-    name: 'TINA BROWN',
+    name: 'Saurab killa',
     status: 'HR EXECUTIVE'
   }
 ];
